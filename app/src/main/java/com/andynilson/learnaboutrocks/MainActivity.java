3583cfc5d69e54e34983f2d77c72358847f3c156
Andy Nilson <andy@andynilson.com>
2016-03-12 08:32:11 -0800

    initial commit

---
+++ app/src/main/java/com/andynilson/learnaboutrocks/MainActivity.java
@@ -0,0 +1,29 @@
+package com.andynilson.learnaboutrocks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void navigateToActivity(View view) {
+        Intent intent = null;
+
+        if (view.getId() == R.id.igneous_button) {
+            intent = new Intent(this, IgneousActivity.class);
+        } else if (view.getId() == R.id.metamorphic_button) {
+            intent = new Intent(this, MetamorphicActivity.class);
+        } else if (view.getId() == R.id.sedimentary_button) {
+            intent = new Intent(this, SedimentaryActivity.class);
+        }
+
+        startActivity(intent);
+    }
+}
